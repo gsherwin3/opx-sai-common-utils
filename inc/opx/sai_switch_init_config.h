26f1a76924c0e0b9df6f45b35aef06b1b74ad6ed
Gordon Sherwin <gsherwin@extremenetworks.com>
2017-01-31 16:10:13 -0500

    Temp fix will address in snapcraft_vm.yaml

--- inc/opx/sai_switch_init_config.h
+++ inc/opx/sai_switch_init_config.h
@@ -35,8 +35,8 @@
  * @todo File path should be passed from Adapter Host and this
  * macro should be removed.
  */
-/* EXTREME_HACK #define SAI_INIT_CONFIG_FILE               "/etc/opx/sai/init.xml" */
-#define SAI_INIT_CONFIG_FILE               "/etc/opx/dell-s6000-vm/sai/init.xml"
+/* EXTREME_HACK temp address in snapcraft_vm.yaml #define SAI_INIT_CONFIG_FILE               "/etc/opx/sai/init.xml" */
+#define SAI_INIT_CONFIG_FILE               "/vendor/dell/s6000-vm/etc/opx/sai/init.xml"
 
 /**
  * @def SAI_ATTR_INFO_TYPE
